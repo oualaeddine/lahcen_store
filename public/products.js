7e5c1d1da8017e66ca09bc9f718348a671d81749
hiba.ouldsetti@gmail.com <hiba.ouldsetti@gmail.com>
2019-10-02 20:37:40 +0100

    display data on update form

---
+++ public/products.js
@@ -0,0 +1,33 @@
+var db = firebase.firestore();
+
+
+// Get products List
+db.collection("products").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+        var data = doc.data();
+        var mrow = "<tr><td>" + data.name + "</td><td>" + data.price + "</td><td>" + data.buyPrice + "</td><td>" +
+            data.quantity +
+            "</td><td> <button class='btn-info btn' data-toggle='modal' data-target='#exampleModal' data-book-id="+data.id+">Modifier</button> <button class='btn-danger btn'>Suprimer</button>" 
+ 
+            "</td></tr>";
+        console.log(mrow);
+
+        $("#all_prod").append(mrow)
+
+    });
+});
+
+$('#exampleModal').on('show.bs.modal', function(e) {
+    var id = $(e.relatedTarget).data('book-id');
+    var docRef = db.collection("products").doc(""+id+"");
+    docRef.get().then(function(doc) {
+           var data = doc.data();
+           $(e.currentTarget).find('input[name="product_name"]').val(data.name);
+           $(e.currentTarget).find('input[name="product_price"]').val(data.price);
+           $(e.currentTarget).find('input[name="buy_price"]').val(data.buyPrice);
+           $(e.currentTarget).find('input[name="quantity"]').val(data.quantity);
+           
+    
+});
+});
