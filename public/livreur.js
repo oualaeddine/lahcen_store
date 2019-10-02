ca94fd09cadf88656b8949612a8c72c21b0f5b62
hiba.ouldsetti@gmail.com <hiba.ouldsetti@gmail.com>
2019-10-02 16:12:07 +0100

    livreur

--- public/livreur.js
+++ public/livreur.js
@@ -9,7 +9,7 @@ db.collection("delivery_men").get().then((querySnapshot) => {
         console.log(doc.data());
         var data = doc.data();
         var mrow = "<tr><td>" + data.name + "</td><td>" + data.phone + "</td><td>" + data.email + "</td><td>" +
-            data.address + "</td><td>" + data.city + "</td><td>" + data.delivered_orders  + "</td><td>" + data.assigned_orders +
+            data.address + "</td><td>" + data.city +
             "</td><td> <button class='btn-info btn'>infos</button></td></tr>";
         console.log(mrow);
 
